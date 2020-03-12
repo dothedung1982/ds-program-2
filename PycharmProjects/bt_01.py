5d0f34619429f5c8a2f69e436c81bec98e4f4058
dothedung1982 <58157897+dothedung1982@users.noreply.github.com>
2020-03-12 19:40:16 +0700

    Add bt_01.py

---
+++ PycharmProjects/bt_01.py
@@ -0,0 +1,7 @@
+# Write a Python program to get the Python version you are using.
+import sys
+print("bt_01: Write a Python program to get the Python version you are using.=======")
+print("Python version")
+print(sys.version)
+print("Version info.")
+print(sys.version_info)
